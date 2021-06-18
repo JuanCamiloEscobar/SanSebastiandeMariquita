9678400acbd3c9acc924c235b31afa0af115a533
JuanCamiloEscobar <juancamiloe18@gmail.com>
2021-06-17 23:06:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sansebastiandemariquita/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sansebastiandemariquita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/sansebastiandemariquita/Splash.java
@@ -0,0 +1,36 @@
+package com.example.sansebastiandemariquita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    MediaPlayer sonidoCascada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        sonidoCascada= MediaPlayer.create(this,R.raw.cascada);
+        sonidoCascada.start();
+
+        TimerTask inicio = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicio,7000);
+    }
+}
